d97648407a4f49c7bbb2a4aa49eb862d09bc3f6e
Dmitrii Kamaldinov <dimak24.05@mail.ru>
2019-04-22 23:06:45 +0300

    vcpkg

--- vcpkg/ports/ppconsul/portfile.cmake
+++ vcpkg/ports/ppconsul/portfile.cmake
@@ -4,15 +4,20 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO oliora/ppconsul
     REF e32cf6f80b598760278c018eed546f88292f1735
-    SHA512 8ca3b1f37a22bf27ce27af27676ba88a67c0c7b3357488edb925649b5d59b546b5cbf7a14452d07b41bb2649de7d0a8126d247a797f1a0e7395c3704fb54a758
+    SHA512 8ca3b1f37a22bf27ce27af27676ba88a67c0c7b3357488edb925649b5d59b546b5cbf7a14452d07b41bb2649de7d0a8126d247a797f$
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
-        -DCMAKE_USE_OPENSSL=ON
 )
 vcpkg_install_cmake()
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_copy_pdbs()
